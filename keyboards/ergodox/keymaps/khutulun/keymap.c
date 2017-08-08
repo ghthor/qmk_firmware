7daa7391c42356498906e8762bda0292b1b2cd81
Wilhelmina Drengwitz <ghthor@gmail.com>
2017-08-08 11:34:31 -0400

    Add L & R Brackets to base layer mappings

--- keyboards/ergodox/keymaps/khutulun/keymap.c
+++ keyboards/ergodox/keymaps/khutulun/keymap.c
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |      |      | LGui |                                       |      |      |      |      |      |
+ *   |  [   |      |      |      | LGui |                                       |      |      |      |      |   ]  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,---------------.
  *                                        |      |      |       |      |        |
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,         KC_W,    KC_E,    KC_R,    KC_T,   KC_TRNS,
         KC_GRV,         KC_A,         KC_S,    KC_D,    KC_F,    KC_G,
         KC_LSFT,        CTL_T(KC_Z),  KC_X,    KC_C,    KC_V,    KC_B,   KC_TRNS,
-        KC_TRNS,        KC_TRNS,      KC_TRNS, KC_LALT, KC_LGUI,
+        KC_LBRC,        KC_TRNS,      KC_TRNS, KC_LALT, KC_LGUI,
 
                                                       KC_TRNS, KC_TRNS,
                                                                KC_TRNS,
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS,      KC_Y,    KC_U,     KC_I,    KC_O,    KC_P,           KC_BSLS,
                       KC_H,    KC_J,     KC_K,    KC_L,    KC_SCLN,        KC_QUOT,
         KC_TRNS,      KC_N,    KC_M,     KC_COMM, KC_DOT,  CTL_T(KC_SLSH), KC_RSFT,
-                               KC_RGUI,  KC_RALT, KC_TRNS, KC_TRNS,        KC_TRNS,
+                               KC_RGUI,  KC_RALT, KC_TRNS, KC_TRNS,        KC_RBRC,
 
         KC_TRNS,  KC_TRNS,
         KC_TRNS,
