bb4275400b1712f9c3340de254a79d4228d96af8
Wilhelmina Drengwitz <ghthor@gmail.com>
2018-01-29 10:56:43 -0500

    Add binding for ESC key

--- keyboards/ergodox_ez/keymaps/khutulun/keymap.c
+++ keyboards/ergodox_ez/keymaps/khutulun/keymap.c
@@ -12,7 +12,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   =    |   1  |   2  |   3  |   4  |   5  |      |           |      |   6  |   7  |   8  |   9  |   0  |   -    |
+ * |   =    |   1  |   2  |   3  |   4  |   5  | Esc  |           |      |   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |  Tab   |   Q  |   W  |   E  |   R  |   T  |      |           |      |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [BASE] = KEYMAP(
         // left hand
-        KC_EQL,         KC_1,         KC_2,    KC_3,    KC_4,    KC_5,   KC_TRNS,
+        KC_EQL,         KC_1,         KC_2,    KC_3,    KC_4,    KC_5,   KC_ESC,
         KC_TAB,         KC_Q,         KC_W,    KC_E,    KC_R,    KC_T,   KC_TRNS,
         KC_GRV,         KC_A,         KC_S,    KC_D,    KC_F,    KC_G,
         KC_LSFT,        CTL_T(KC_Z),  KC_X,    KC_C,    KC_V,    KC_B,   KC_LSFT,
