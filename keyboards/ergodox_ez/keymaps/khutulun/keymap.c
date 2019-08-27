f485ad88991d5a5ddf5226780454530a7c87ebfb
Willa Drengwitz <ghthor@gmail.com>
2019-08-27 15:49:27 -0400

    Add wow mappings to preserve functionality

--- keyboards/ergodox_ez/keymaps/khutulun/keymap.c
+++ keyboards/ergodox_ez/keymaps/khutulun/keymap.c
@@ -4,13 +4,15 @@
 #include "version.h"
 
 #define BASE 0
-#define CHRD 1
+#define WOWW 1
 #define SYSK 2
+#define CHRD 3
 
 // Unused
 #define OSM_LSFT = SFT_T(OSM(MOD_LSFT))
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
@@ -46,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                               KC_SPC, KC_BSPC, KC_DOWN,
 
         // right hand
-        KC_TRNS,      KC_6,    KC_7,     KC_8,    KC_9,    KC_0,           KC_MINS,
+       TG(WOWW),      KC_6,    KC_7,     KC_8,    KC_9,    KC_0,           KC_MINS,
        MO(SYSK),      KC_Y,    KC_U,     KC_I,    KC_O,    KC_P,           KC_BSLS,
                       KC_H,    KC_J,     KC_K,    KC_L,    KC_SCLN,        KC_QUOT,
         KC_RSFT,      KC_N,    KC_M,     KC_COMM, KC_DOT,  CTL_T(KC_SLSH), KC_RSFT,
@@ -57,35 +59,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_RGHT, KC_TRNS, KC_ENT
     ),
 
-/* Keymap 1: Chord Layer
+/* Keymap 1: Wow Layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   =    |   1  |   2  |   3  |   4  |   5  |      |           |      |   6  |   7  |   8  |   9  |   0  |   -    |
+ * |   =    |  F1  |  F2  |  F3  |   7  |   8  | Esc  |           |      |   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |  Tab   |   Q  |   W  |   E  |   R  |   T  |      |           |      |   Y  |   U  |   I  |   O  |   P  |   \    |
+ * |  Tab   |   Q  |   W  |   E  |   5  |   6  |      |           |      |   Y  |   U  |   I  |   O  |   P  |   \    |
+ * |--------+------+------+------+------+------|  0   |           |      |------+------+------+------+------+--------|
+ * |  Grv   |   A  |   S  |   D  |   3  |   4  |------|           |------|   H  |   J  |   K  |   L  |  ;   |    '   |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |  Grv   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;   |    '   |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | LShft  |Z/Ctrl|   X  |   C  |   V  |   B  | LShf |           | RShf |   N  |   M  |   ,  |   .  |//Ctrl| RShft  |
+ * | LShft  |Z/Ctrl|   X  |   C  |   1  |   2  |  9   |           | RShf |   N  |   M  |   ,  |   .  |//Ctrl| RShft  |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |  [   |      |      |      | LGui |                                       |      |      |      |      |   ]  |
+ *   |  [   | F4   | LCtl | LAlt | LGui |                                       | RGui | LAlt |      |      |   ]  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,---------------.
- *                                        |      |      |       |      |        |
+ *                                        |  Del | PgDn |       | PgUp |        |
  *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      |      |       |      |        |      |
+ *                                 |      |      | Left |       |  Up  |        |      |
  *                                 | Spc  | Bcps |------|       |------|        | Ent  |
- *                                 |      |      |      |       |      |        |      |
+ *                                 |      |      | Down |       | Rght |        |      |
  *                                 `--------------------'       `----------------------'
  */
 
-[CHRD] = LAYOUT_ergodox(
+[WOWW] = LAYOUT_ergodox(
         // left hand
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS,   KC_F1,   KC_F2,   KC_F3,    KC_7,    KC_8, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_5,    KC_6,    KC_0,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_3,    KC_4,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_1,    KC_2,    KC_9,
+        KC_TRNS, KC_TRNS, KC_LCTL, KC_TRNS, KC_TRNS,
 
                                                      KC_TRNS, KC_TRNS,
                                                               KC_TRNS,
@@ -117,6 +119,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
         // right hand
           KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+
+        KC_TRNS, KC_TRNS,
+        KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS
+    ),
+
+[CHRD] = LAYOUT_ergodox(
+        // left hand
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+
+                                                     KC_TRNS, KC_TRNS,
+                                                              KC_TRNS,
+                                            KC_TRNS, KC_TRNS, KC_TRNS,
+
+        // right hand
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
